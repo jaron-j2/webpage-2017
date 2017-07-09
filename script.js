189873bb2a8082aa90ce30b2a25cfab6680b7187
Jaron Chan Jin Jia <jaron.chan.jin.jia@gmail.com>
2017-07-10 02:35:53 +0800

    Toggle expand buttons

--- script.js
+++ script.js
@@ -3,7 +3,17 @@ $(document).ready(function() {
   $('.carousel').carousel("pause")
 
   $("#expandSkill").click(function(){
-        $("#moreSkill").slideToggle("slow")
-    });
+    $("#moreSkill").slideToggle("slow");
+  });
+
+  $("#expandSkill").on({
+    'click': function() {
+     var src = ($(this).attr('src') === 'img/expand-icon.png')
+     ? 'img/collapse-icon.png'
+     : 'img/expand-icon.png'
+     $(this).attr('src', src);
+    }
+  });
 });
+
 
