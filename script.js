35c3317cef8b086d9738f99994e0482085927ee5
Jaron Chan Jin Jia <jaron.chan.jin.jia@gmail.com>
2017-07-12 12:08:33 +0800

    Added sections to CS3216

--- script.js
+++ script.js
@@ -14,6 +14,32 @@ $(document).ready(function() {
      $(this).attr('src', src);
     }
   });
+
+  $("#expandMarket").click(function(){
+    $(".more-market").slideToggle("slow");
+  });
+
+  $("#expandMarket").on({
+    'click': function() {
+     var src = ($(this).attr('src') === '../img/expand-icon.png')
+     ? '../img/collapse-icon.png'
+     : '../img/expand-icon.png'
+     $(this).attr('src', src);
+   }
+  });
+
+  $("#expandDesign").click(function(){
+    $(".more-design").slideToggle("slow");
+  });
+
+  $("#expandDesign").on({
+    'click': function() {
+     var src = ($(this).attr('src') === '../img/expand-icon.png')
+     ? '../img/collapse-icon.png'
+     : '../img/expand-icon.png'
+     $(this).attr('src', src);
+    }
+  });
 });
 
 
